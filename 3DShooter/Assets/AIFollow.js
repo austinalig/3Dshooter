0e73937570f79995165e3e954ea508a299441088
KickSave35 <kscorcia@knights.ucf.edu>
2015-04-08 22:18:03 -0400

    Level Texture, Health Bar, Crosshair
    Health Bar can be done once we get the enemy shooting, and the crosshair
    is off a little but I can reposition it a little but would rather fix
    the gun rotation so its more towards the middle of the screen, and still
    looks good in the game.

---
+++ 3DShooter/Assets/AIFollow.js
@@ -0,0 +1,26 @@
+﻿var target: Transform;
+var moveSpeed = 3;
+var rotationSpeed = 3;
+var myTransform: Transform;
+
+function Awake()
+{
+	
+	myTransform = transform;
+	
+}
+
+function Start()
+{
+
+	target = GameObject.FindWithTag("Player").transform;
+
+}
+
+function Update()
+{
+
+	transform.LookAt(Vector3(target.position.x, transform.position.y, target.position.x));
+	myTransform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+
+}
