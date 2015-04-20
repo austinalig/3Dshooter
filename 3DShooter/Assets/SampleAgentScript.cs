b2c96452bde3c0f4d6a6366b29a6ef0b1a4ab979
austinalig <austinalig@knights.ucf.edu>
2015-04-20 08:44:57 -0700

    new
    stuff

---
+++ 3DShooter/Assets/SampleAgentScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SampleAgentScript : MonoBehaviour {
+	public int seekrad;
+	public Transform target;
+	NavMeshAgent agent;
+	public int health = 5;
+	private int currenthealth;
+	public GameObject bullet;
+
+	// Use this for initialization
+	void Start () {
+		currenthealth = health;
+		agent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ((agent.transform.position - target.position).magnitude > seekrad)
+		agent.SetDestination (target.position);
+		else agent.SetDestination (agent.transform.position);
+	}
+	void OnTriggerEnter(Collider other) {
+		if (other.tag == bullet.tag) {
+			currenthealth = currenthealth - 1;
+			if (currenthealth <= 0)
+				Destroy (gameObject);
+		} else
+			Debug.Log (other.tag);
+	}
+}
